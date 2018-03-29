6f0156b4b25ed1aadc7ffd002daa898b79e08d8a
Shahedul Khandkar <shahedk@gmail.com>
2018-03-28 22:22:50 -0400

    Adding web api

---
+++ Browser.WebApi/Controllers/RawHtmlController.cs
@@ -0,0 +1,22 @@
+﻿using Browser.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Browser.WebApi.Controllers
+{
+    [Route("/RawHtml")]
+    public class RawHtmlController : Controller
+    {
+        private readonly IBrowserService _browserService;
+        public RawHtmlController(IBrowserService browserService)
+        {
+            _browserService = browserService;
+        }
+
+        [HttpGet("{url}")]
+        public FileResult Get(string url, string script = "", int width = 1200, int height = 900)
+        {
+            var content = _browserService.GetRawHtml(url, script, height, width);
+            return PhysicalFile(content.ScreenShotPath, "text/html", "screenshot.png");
+        }
+    }
+}
---
+++ Browser.WebApi/Controllers/ScreenShotController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Browser.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Browser.WebApi.Controllers
+{
+    [Route("/Screenshot")]
+    public class ScreenShotController : Controller
+    {
+        private readonly IBrowserService _browserService;
+
+        public ScreenShotController( IBrowserService browserService)
+        {
+            _browserService = browserService;
+        }
+
+        [HttpGet("{url}")]
+        public FileResult Get(string url, string script = "", int width=1200, int height=900)
+        {
+            var content = _browserService.TakeScreenShot(url, script, height, width);
+
+            return PhysicalFile(content.ScreenShotPath, "image/png", "screenshot.png");
+        }
+    }
+}
